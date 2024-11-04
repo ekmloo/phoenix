3aec50d658337b5cc23a7eaf0656e5983bc4f185
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-04 17:57:28 +0100

    Create solana.js

---
+++ utils/solana.js
@@ -0,0 +1,16 @@
+// utils/solana.js
+const { Keypair } = require('@solana/web3.js');
+
+/**
+ * Creates a new Solana wallet (Keypair).
+ * @returns {Object} An object containing the public key and private key array.
+ */
+const createWallet = () => {
+  const keypair = Keypair.generate();
+  return {
+    publicKey: keypair.publicKey.toBase58(),
+    privateKey: Array.from(keypair.secretKey)
+  };
+};
+
+module.exports = { createWallet };
