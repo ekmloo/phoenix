9747473f77626d06a2d94ae4b5502bd7eb3d9626
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-06 08:12:21 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   telegramId: { type: Number, required: true, unique: true },
   walletPublicKey: { type: String, required: true, unique: true },
-  walletPrivateKey: { type: [Number], required: true }, // Stored as an array of numbers
-  connectedPhantom: { type: String } // Optional: Store Phantom wallet address if connected
+  walletPrivateKey: { type: String, required: true }, // Encrypted private key stored as string
 });
 
 module.exports = mongoose.model('User', userSchema);
