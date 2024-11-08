d102ce89d590c8016f87e8b4b5c6a27e5931d268
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-08 18:36:43 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   telegramId: { type: Number, required: true, unique: true },
-  walletPublicKey: { type: String, required: true, unique: true },
-  walletPrivateKey: { type: String, required: true }, // Encrypted private key stored as string
+  walletPublicKey: { type: String, unique: true },
+  walletPrivateKey: { type: String }, // Encrypted private key
+  referredBy: { type: Number }, // Telegram ID of referrer
+  referrals: { type: Number, default: 0 }, // Number of referrals
+  last4: { type: String }, // Last 4 characters of public key
 });
 
 module.exports = mongoose.model('User', userSchema);
