d3e53f4cd64b8aacb3ff81c604edf3f5dc0bb923
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-09 11:15:28 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const bumpbotSchema = new mongoose.Schema({
   contractAddress: { type: String, required: true },
-  bumpbotWallet: { type: String, required: true },
+  amountSOL: { type: Number, required: true },
   active: { type: Boolean, default: false },
 });
 
@@ -13,6 +13,8 @@ const userSchema = new mongoose.Schema({
   walletPrivateKey: { type: String }, // Encrypted private key
   referredBy: { type: Number }, // Telegram ID of referrer
   referrals: { type: Number, default: 0 }, // Number of referrals
+  customWalletPublicKey: { type: String }, // Custom wallet public key
+  customWalletPrivateKey: { type: String }, // Encrypted custom wallet private key
   bumpbots: [bumpbotSchema], // Array of bumpbots
 });
 
