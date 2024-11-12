b920d344daf88f2c21fe135729297e259c5bf3da
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-12 18:23:35 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,6 +6,13 @@ const userSchema = new mongoose.Schema({
   walletPublicKey: { type: String },
   walletPrivateKey: { type: String },
   referredBy: { type: Number },
+  referralEarnings: { type: Number, default: 0 }, // Total SOL earned from referrals
+  token: {
+    name: String,
+    ticker: String,
+    photoUrl: String,
+    mintAddress: String,
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
