5fe1f3d33fbb5a1899ae53a89f83f7e755c78b38
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-12 18:31:45 +0100

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -5,7 +5,7 @@ module.exports = (bot) => {
 
 Phoenix Bot allows you to create and manage your Solana wallets directly within Telegram. Invite friends using your referral link and earn a 50% commission on their transaction fees when they perform paid actions!
 
-*Transfer Fee:* A 0.1% fee is applied to all transactions, and a 0.9% fee on scheduled transfers.
+*Transfer Fee:* A 0.1% fee is applied to all transactions.
 
 Join our community: [t.me/phoenixbotsol](https://t.me/phoenixbotsol)`;
     await ctx.replyWithMarkdown(aboutMessage);
