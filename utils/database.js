356a676f8e332faacebcd36579d2d17b274c2a58
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-12 17:52:39 +0100

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -3,26 +3,14 @@ const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  console.error('[-] MONGODB_URI is not defined in environment variables.');
-  throw new Error('MONGODB_URI is not defined');
-}
-
-// Suppress Mongoose Deprecation Warning
 mongoose.set('strictQuery', true);
 
-// Connect to Database
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log('[+] MongoDB Connected');
   } catch (error) {
     console.error('[-] MongoDB Connection Error:', error);
-    // Do not exit the process; throw the error to be handled upstream
-    throw error;
   }
 };
 
