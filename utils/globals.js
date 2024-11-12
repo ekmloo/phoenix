6a5d67eee1f5a94f21a0adcdd24c5747e3454afd
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-12 17:53:10 +0100

    Update globals.js

--- utils/globals.js
+++ utils/globals.js
@@ -3,18 +3,8 @@ const { Connection, clusterApiUrl, Keypair } = require('@solana/web3.js');
 
 const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
 
-// Initialize Bot's Keypair
-let botKeypair;
-try {
-  const BOT_WALLET_PRIVATE_KEY = process.env.BOT_WALLET_PRIVATE_KEY; // JSON array as string
-  if (!BOT_WALLET_PRIVATE_KEY) {
-    throw new Error('BOT_WALLET_PRIVATE_KEY is not defined in environment variables.');
-  }
-  const botSecretKey = Uint8Array.from(JSON.parse(BOT_WALLET_PRIVATE_KEY));
-  botKeypair = Keypair.fromSecretKey(botSecretKey);
-} catch (error) {
-  console.error('[-] Invalid BOT_WALLET_PRIVATE_KEY. It should be a JSON array of numbers.');
-  throw error;
-}
+const BOT_WALLET_PRIVATE_KEY = process.env.BOT_WALLET_PRIVATE_KEY;
+const botSecretKey = Uint8Array.from(JSON.parse(BOT_WALLET_PRIVATE_KEY));
+const botKeypair = Keypair.fromSecretKey(botSecretKey);
 
 module.exports = { connection, botKeypair };
