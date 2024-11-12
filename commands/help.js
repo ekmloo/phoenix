052655d2fedb734624b36251bf42a1fd00ad28fe
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-12 18:05:55 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,21 +1,15 @@
 // commands/help.js
 module.exports = (bot) => {
-  bot.command('help', async (ctx) => {
+  bot.help(async (ctx) => {
     const helpMessage = `
-*Available Commands:*
-/start - Start the bot
-/referral - Get your referral link
-/about - About the bot
-/wallet [referral_code] - Create or view your Solana wallet. Optionally include a referral code.
-/customwallet - Create a custom wallet.
-/balance - Check your wallet balance
-/send <address> <amount> - Send SOL to another address
-/schedule <address> <amount> <delay_in_minutes> - Schedule a transaction to send SOL after a delay
-/bumpbot start <contract_address> <amount_in_SOL> - Start the bumpbot for a specific token
-/bumpbot stop <contract_address> - Stop the bumpbot for a specific token
-/help - Show this help message
+📋 *Available Commands:*
 
-Join our community: [t.me/phoenixbotsol](https://t.me/phoenixbotsol)
+/start [referral_code] - Start the bot and optionally use a referral code.
+/help - Show this help message.
+/about - Learn more about Phoenix Bot.
+/wallet - Create or view your Solana wallet.
+/balance - Check your wallet balance.
+/send <address> <amount> - Send SOL to another address.
     `;
     await ctx.replyWithMarkdown(helpMessage);
   });
