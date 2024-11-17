4a0035df00793dc23d3e7a198c6a77a4cbfe6437
ekmloo <147192479+ekmloo@users.noreply.github.com>
2024-11-17 14:15:55 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,24 +2,11 @@
 
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  telegramId: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  walletPublicKey: {
-    type: String,
-    default: null, // Initially null until wallet is created
-  },
-  walletPrivateKey: {
-    type: Buffer, // Store as binary data
-    default: null, // Initially null until wallet is created
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+const userSchema = new mongoose.Schema({
+  telegramId: { type: Number, required: true, unique: true },
+  walletPublicKey: { type: String, required: true },
+  walletPrivateKey: { type: String, required: true }, // Should be String to store encrypted data
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
