b675cc336e3d0557711edbc204071995201b6b3a
ekmloo <ekmelk3a@gmail.com>
2024-11-18 18:44:22 +0100

    fix: use single bot instance from index.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,30 +1,6 @@
 // api/webhook.js
 
-const { Telegraf, Scenes: { Stage }, session } = require('telegraf');
-const startCommand = require('../commands/start');
-const sendScene = require('../commands/send');
-
-const BOT_TOKEN = process.env.BOT_TOKEN;
-
-if (!BOT_TOKEN) {
-  console.error('BOT_TOKEN is not defined.');
-  process.exit(1);
-}
-
-const bot = new Telegraf(BOT_TOKEN);
-
-// Create stage for scenes
-const stage = new Stage([sendScene]);
-
-// Register middleware and scenes
-bot.use(session());
-bot.use(stage.middleware());
-
-// Register /start command
-bot.command(startCommand.command, startCommand.execute);
-
-// Register /send command to enter the scene
-bot.command('send', (ctx) => ctx.scene.enter('send-wizard'));
+const bot = require('../index');
 
 // Export the webhook handler
 module.exports = (req, res) => {
